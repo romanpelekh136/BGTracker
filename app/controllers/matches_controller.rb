2c2c03dc4d913c4136ec62c0df7c7833bdd6b552
romanpelekh136 <rpeleh19@gmail.com>
2026-08-29 18:18:42 +0300

    Refactor match creation form to use instance variables for board games and players, enhance data handling in views

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -2,6 +2,8 @@ class MatchesController < ApplicationController
   def new
     @match = Match.new
     @match.match_results.build
+    @board_games = BoardGame.order(:title)
+    @players = Player.order(:username)
   end
 
   def index
@@ -14,16 +16,17 @@ class MatchesController < ApplicationController
 
   def create
     @match = Match.new(match_params)
-
     if @match.save
       redirect_to @match, notice: "Match saved successfully."
     else
+      @board_games = BoardGame.order(:title)
+      @players = Player.order(:username)
       render :new, status: :unprocessable_entity
     end
   end
 
   def show
-    @match = Match.find(params[:id])
+    @match = Match.includes(:board_game, match_results: :player).find(params[:id])
   end
 
   def destroy
@@ -35,7 +38,7 @@ class MatchesController < ApplicationController
     params.require(:match).permit(
       :board_game_id,
       :played_at,
-      match_results_attributes: [ :id, :player_id, :score, :rank, :_destroy ]
+      match_results_attributes: [ :id, :player_id, :score, :rank ]
       )
   end
 end
